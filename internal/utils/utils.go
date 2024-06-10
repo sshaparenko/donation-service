4ade415fff15e929d159304a53cca78c7856270d
sshaparenko <zshaparenkoz@gmail.com>
2024-06-10 12:41:09 +0300

    change path to .env file in utils GetValue function

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -11,7 +11,7 @@ import (
 GetValue reads .env and loading env varables
 */
 func GetValue(key string) string {
-	err := godotenv.Load(".env")
+	err := godotenv.Load("../../.env")
 	if err != nil {
 		log.Fatalf("Error loading .env file\n")
 		log.Fatalf(err.Error())
